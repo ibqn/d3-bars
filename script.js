07d00fb727b408f98e3eca6e5a30d66f91ce7181
Evgeny Bobkin <evgeny.bobkin@gmail.com>
2017-01-16 16:09:40 +0100

    wrap script content in a function

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-var data = [4, 8, 15, 16, 23, 42];
+(function() {
+  var data = [4, 8, 15, 16, 23, 42];
 
-var xScale = d3.scaleLinear()
+  var xScale = d3.scaleLinear()
     .domain([0, d3.max(data)])
     .range([0, 420]);
 
-d3.select(".chart")
-  .selectAll("div")
-  .data(data)
-  .enter().append("div")
-  .style("width", function(d) { return xScale(d) + "px"; })
-  .text(function(d) { return d; });
+  d3.select(".chart")
+    .selectAll("div")
+    .data(data)
+    .enter().append("div")
+    .style("width", function(d) { return xScale(d) + "px"; })
+    .text(function(d) { return d; });
+})();
