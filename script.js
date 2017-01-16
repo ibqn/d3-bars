0b916b8ec2ab00e4ac7611fd32e34850a45e524d
Evgeny Bobkin <evgeny.bobkin@gmail.com>
2017-01-16 16:59:54 +0100

    fix identation

--- script.js
+++ script.js
@@ -14,19 +14,19 @@
     chart.attr("height", barHeight * data.length);
 
     var bar = chart.selectAll("g")
-        .data(data)
+      .data(data)
       .enter().append("g")
-        .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+      .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
 
     bar.append("rect")
-        .attr("width", function(d) { return xScale(d.value); })
-        .attr("height", barHeight - 1);
+      .attr("width", function(d) { return xScale(d.value); })
+      .attr("height", barHeight - 1);
 
     bar.append("text")
-        .attr("x", function(d) { return xScale(d.value) - 3; })
-        .attr("y", barHeight / 2)
-        .attr("dy", ".35em")
-        .text(function(d) { return d.value; });
+      .attr("x", function(d) { return xScale(d.value) - 3; })
+      .attr("y", barHeight / 2)
+      .attr("dy", ".35em")
+      .text(function(d) { return d.value; });
   });
 
   function type(d) {
