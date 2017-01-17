ec257a78d5b0b77da8c267438ac4e7faf392ecd6
Evgeny Bobkin <evgeny.bobkin@gmail.com>
2017-01-18 00:17:21 +0100

    add axis

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 (function() {
-  var width = 960,
-      height = 500;
+  var margin = {top: 20, right: 30, bottom: 30, left: 40},
+      width = 960 - margin.left - margin.right,
+      height = 500 - margin.top - margin.bottom;
 
   var yScale = d3.scaleLinear()
     .range([height, 0]);
@@ -10,10 +11,15 @@
     .padding(0.025);
 
   var chart = d3.select(".chart")
-    .attr("width", width)
-    .attr("height", height);
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
 
   d3.tsv("data.tsv", type, function(error, data) {
+    if (error) throw error;
+
     xScale.domain(data.map(function(d) { return d.name; }));
     yScale.domain([0, d3.max(data, function(d) { return d.value; })]);
 
@@ -28,6 +34,7 @@
       .attr("width", xScale.bandwidth());
 
     bar.append("text")
+      .classed("bars", true)
       .attr("x", xScale.bandwidth() / 2)
       .classed("blue", function(d) {
         if ((height - yScale(d.value)) < 16) {
@@ -44,7 +51,20 @@
         return v + s;
       })
       .attr("dy", ".75em")
-      .text(function(d) { return math.round(d.value, 4); });
+      .text(function(d) { return math.round(100 * d.value, 2); });
+
+    var xAxis = d3.axisBottom(xScale);
+    var yAxis = d3.axisLeft(yScale)
+      .ticks(10, '%');
+
+    // Add the x Axis
+    chart.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+
+    // Add the y Axis
+    chart.append("g")
+      .call(yAxis);
   });
 
   function type(d) {
